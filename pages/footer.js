a3f63f490a105c676d913d736159e0d74dd0da05
LauTa <lautaguzman21@hotmail.com>
2024-10-28 00:15:35 -0300

    estilos para la informacion del dolar y prueba de estilos para footer en mediaquery mobile

--- pages/footer.js
+++ pages/footer.js
@@ -1,27 +1,16 @@
 const footer = document.querySelector("#footer");
 
-const footerInfo = document.createElement("div");
-footerInfo.className = `footer-info`;
-footer.append(footerInfo);
-
-const infoPage = document.createElement("span");
-infoPage.innerHTML = `<i class="fa-solid fa-info" style="color: #00635c;"></i>`;
-footerInfo.append(infoPage);
-
-const footerRedes = document.createElement("div");
-footerRedes.className = `footer-redes`;
-footer.append(footerRedes);
-
-const redesTitle = document.createElement("h4");
-redesTitle.innerText = `seguime en mis redes 😉`;
-footerRedes.append(redesTitle);
+const footerTitle = document.createElement("h4");
+footerTitle.innerText = `seguime en mis redes 😉`;
+footer.append(footerTitle);
 
 const redesContainer = document.createElement("ul");
 redesContainer.className = `redes-container`;
-footerRedes.append(redesContainer);
+footer.append(redesContainer);
 
 // ARRAY QUE CONTIENE LOS ICONOS DE MIS REDES
 const iconosRedes = [
+  `<i class="fa-solid fa-info fa-xl" style="color: #00635c;"></i>`,
   `<i class="fa-brands fa-square-github fa-xl" style="color: #24292e;"></i>`,
   `<i class="fa-brands fa-linkedin fa-xl" style="color: #0077b5;"></i>`,
   `<i class="fa-brands fa-square-instagram fa-xl" style="color: #e1306c;"></i>`,
