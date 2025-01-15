163d96062599f85af05621ea145fb0350b28c7d5
LauTa <lautaguzman21@hotmail.com>
2025-01-14 21:13:03 -0300

    modificaciones en about the site ; funcion y css

--- scripts/aboutTheSite.js
+++ scripts/aboutTheSite.js
@@ -13,12 +13,24 @@ function aboutTheSite() {
   closeInfoSite.addEventListener("click", () => {
     infoSiteContainer.style.display = "none";
   });
+  const descriptionSite = document.createElement("ul");
+  descriptionSite.className = "description-container";
+  descriptionSite.innerHTML = `
+  <li>
+  esta página permite consultar el valor de compra y venta del dólar en argentina, mostrando información detallada sobre cada tipo dólar disponible.
+  </li>
 
-  const descriptionSite = document.createElement("p");
-  descriptionSite.innerHTML = `el objetivo principal de este proyecto es brindar información actualizada sobre el valor del dólar en argentina, incluyendo las distintas cotizaciones existentes y su descripción detallada. los datos se obtienen en tiempo real a través de una <a href="https://dolarapi.com/docs/">api</a> externa, lo que permite mostrar información precisa y actualizada.
-además de ofrecer información sobre las cotizaciones, el sitio incluye una calculadora de conversión de moneda para convertir de ars a usd y viceversa, facilitando la consulta rápida de los valores de cambio.
-este proyecto surgió como parte de mi proceso de aprendizaje continuo en desarrollo web. me propuse el desafío de implementar nuevas herramientas y conceptos, como el consumo de una API que desconocía, la gestión de ramas en gitHub y el desarrollo de funciones en javaScript, como asi también profundicé mis conocimientos en CSS.
-Puedes explorar mi perfil de <a href="https://github.com/lautaguzman">gitHub</a>  para conocer más sobre este y otros proyectos`;
+  <li>
+  incluye una calculadora de conversión de moneda para realizar cambios de ars a usd y viceversa, facilitando la consulta rápida de valores actualizados.
+  los datos se obtienen en tiempo real a través de una <a href="https://dolarapi.com/docs/">api</a> externa, asegurando precisión y actualidad en la información proporcionada.
+  </li>
+
+  <li>
+  esta página fue desarrollada por lautaro guzmán. puedes explorar mi perfil de <a href="https://github.com/lautaguzman">gitHub</a> para conocer más sobre este y otros proyectos.
+  </li>`;
 
   infoSiteContainer.append(closeInfoSite, descriptionSite);
 }
+
+// este proyecto surgió como parte de mi proceso de aprendizaje continuo en desarrollo web. me propuse el desafío de implementar nuevas herramientas y conceptos, como el consumo de una API que desconocía, la gestión de ramas en gitHub y el desarrollo de funciones en javaScript, como asi también profundicé mis conocimientos en CSS.
+// Puedes explorar mi perfil de <a href="https://github.com/lautaguzman">gitHub</a>  para conocer más sobre este y otros proyectos
